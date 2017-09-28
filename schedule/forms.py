f52c7d534c4a0fca6b61140a267219d556bcd6c2
Nikita Marchant <nikita.marchant@gmail.com>
2017-09-28 23:43:31 +0200

    Revert "Limit courses to the ones that have a schedule"
    This reverts commit 572561d61193e302259848d1f28eb9288b75d17e.

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,20 +1,12 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django.db.models import Count
-
-from .models import Selection, MyUser, Course
+from .models import Selection, MyUser
 
 
 class SelectionForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(SelectionForm, self).__init__(*args, **kwargs)
-        qs = Course.objects.annotate(scheds=Count('schedule')).exclude(scheds=0)
-        self.fields['courses'].queryset = qs
-
     class Meta:
         model = Selection
         fields = ['courses', 'name']
-
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
