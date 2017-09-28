009234472a86c8f2f96a147c64ec147d5adf96c5
Nikita Marchant <nikita.marchant@gmail.com>
2017-09-28 22:29:25 +0200

    Limit courses to the ones that have a schedule

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,12 +1,20 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import Selection, MyUser
+from django.db.models import Count
+
+from .models import Selection, MyUser, Course
 
 
 class SelectionForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SelectionForm, self).__init__(*args, **kwargs)
+        qs = Course.objects.annotate(scheds=Count('schedule')).exclude(scheds=0)
+        self.fields['courses'].queryset = qs
+
     class Meta:
         model = Selection
         fields = ['courses', 'name']
+
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
